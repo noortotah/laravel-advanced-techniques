3d778c3be92ef8fd056e54bd130dd95114bfb726
Noor Totah <noor.totah.91@gmail.com>
2019-03-22 23:00:16 +0200

    add accessors and mutators

--- app/Team.php
+++ app/Team.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Team extends Model
 {
     protected $guarded = [];
+
+    public $appends = ['users_count'];
+
+    public function getUsersCountAttribute() //before Retrieving
+    {
+    	return \DB::table('users')
+    					->where('users.team_id', $this->id)
+    					->sum('users.id');
+    }
+
+    public function setTitleAttribute($value) //Before entering database
+    {
+    	$this->attributes['title'] = ucwords($value);
+    }
 }
