1596abbff9462b798e52fbe43044372596910ac5
Noor Totah <noor.totah.91@gmail.com>
2019-03-26 12:47:37 +0300

    add algolia, laravel\scout to Posts and Users

--- app/Post.php
+++ app/Post.php
@@ -2,10 +2,25 @@
 
 namespace App;
 
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+	use Searchable;
+
+	// public function searchableAs()
+	// {
+	// 	return 'posts_index';
+	// }
+
+	// public function toSearchableArray()
+	// {
+	// 	return [
+	// 		'title', 'content'
+	// 	];
+	// }
+
     protected $fillable = [
       'title', 'content', 'published'
   	];
